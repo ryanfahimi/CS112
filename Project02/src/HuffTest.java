6b998a2c47e1ad8f1ad58854a07ca47980ea2fc9
Ryan Fahimi <ryanfahimi@gmail.com>
2023-05-03 15:02:46 -0700

    Updated HuffTest

--- Project02/src/HuffTest.java
+++ Project02/src/HuffTest.java
@@ -1,3 +1,10 @@
+
+/**
+ * This program compares two text files to check if they are identical after filtering disallowed characters.
+ * It is mainly used to test the Huffman coding and decoding process.
+ * @filename HuffTest.java
+ * @author Ryan Fahimi
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -5,19 +12,73 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- * HuffTest is a class that compares an input file and a decoded file character
+ * A class that compares an input file and a decoded file character
  * by character
  * and reports if they match or not.
  */
 public class HuffTest {
 
     /**
-     * The main method that takes two command line arguments: input filename and
-     * decoded filename.
-     * It compares the files and outputs "PASS" if they match or "FAIL" if they
-     * don't.
-     *
-     * @param args command line arguments
+     * A helper class to manage characters and positions while reading files.
+     */
+    private static class Char {
+        private int temp;
+        private int current;
+        private int position;
+
+        /**
+         * Constructor initializes Char with default values.
+         */
+        public Char() {
+            this.temp = 0;
+            this.current = 0;
+            this.position = -1;
+        }
+
+        /**
+         * Checks if the temp character is a disallowed character.
+         * 
+         * @return true if the character is disallowed, false otherwise.
+         */
+        private boolean isDisallowedChar() {
+            return !(this.temp >= '\u0007' && this.temp <= '\u00FE');
+        }
+
+        /**
+         * Checks if the end of the file has been reached.
+         * 
+         * @return true if the end of the file has been reached, false otherwise.
+         */
+        private boolean isEOT() {
+            return (this.temp == -1);
+        }
+
+        /**
+         * Reads the next allowed character from the file.
+         * 
+         * @param reader the BufferedReader to read the character from.
+         * @throws IOException if there's an issue with reading from the file.
+         */
+        private void readNextAllowedChar(BufferedReader reader) throws IOException {
+            boolean eot;
+            do {
+                this.temp = reader.read();
+                eot = isEOT();
+                if (eot) {
+                    break;
+                }
+                this.position++;
+            } while (isDisallowedChar());
+            if (!eot) {
+                this.current = this.temp;
+            }
+        }
+    }
+
+    /**
+     * Main method for running the program.
+     * 
+     * @param args command line arguments: <input filename> <decoded filename>
      */
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -38,77 +99,31 @@ public class HuffTest {
     }
 
     /**
-     * Checks if the given character is allowed or not.
-     *
-     * @param c character to check
-     * @return true if the character is not allowed, false otherwise
-     */
-    private static boolean isDisallowedCharacter(char c) {
-        return !(c >= '\u0007' && c <= '\u00FE');
-    }
-
-    /**
-     * Reads the next allowed character from the given reader.
-     *
-     * @param reader      the reader to read from
-     * @param tempChar    the next character to read
-     * @param currentChar the current character
-     * @param position    the position of the current character
-     * @return an array of integers containing the next character to read, the
-     *         current character, and the position of the current character
-     * @throws IOException if there's an error reading the file
-     */
-    private static int[] readNextAllowedChar(BufferedReader reader, int tempChar, int currentChar, int position)
-            throws IOException {
-        tempChar = reader.read();
-        if (tempChar != -1) {
-            position++;
-            currentChar = tempChar;
-            while (tempChar != -1 && isDisallowedCharacter((char) currentChar)) {
-                int[] result = readNextAllowedChar(reader, tempChar, currentChar, position);
-                tempChar = result[0];
-                currentChar = result[1];
-                position = result[2];
-            }
-        }
-        return new int[] { tempChar, currentChar, position };
-    }
-
-    /**
-     * Compares the given input file and decoded file character by character.
-     *
-     * @param inputFilename   the path of the input file
-     * @param decodedFilename the path of the decoded file
-     * @return true if the files match, false otherwise
-     * @throws IOException if there's an error reading the files
+     * Compares the contents of two files character by character after filtering
+     * disallowed characters.
+     * 
+     * @param inputFilename   the name of the input file.
+     * @param decodedFilename the name of the decoded file.
+     * @return true if the contents of the files are identical, false otherwise.
+     * @throws IOException if there's an issue with reading from the files.
      */
     private static boolean compareFiles(String inputFilename, String decodedFilename) throws IOException {
         try (BufferedReader inputReader = Files.newBufferedReader(Paths.get(inputFilename), StandardCharsets.UTF_8);
                 BufferedReader decodedReader = Files.newBufferedReader(Paths.get(decodedFilename),
                         StandardCharsets.UTF_8)) {
 
-            int[] inputResult;
-            int[] decodedResult;
-            int tempInputChar = 0, tempDecodedChar = 0;
-            int inputChar = 0, decodedChar = 0;
-            int inputPosition = -1, decodedPosition = -1;
+            Char inputChar = new Char();
+            Char decodedChar = new Char();
 
-            while (tempInputChar != -1 || tempDecodedChar != -1) {
-                inputResult = readNextAllowedChar(inputReader, tempInputChar, inputChar,
-                        inputPosition);
-                tempInputChar = inputResult[0];
-                inputChar = inputResult[1];
-                inputPosition = inputResult[2];
+            while (!inputChar.isEOT() && !decodedChar.isEOT()) {
+                inputChar.readNextAllowedChar(inputReader);
 
-                decodedResult = readNextAllowedChar(decodedReader, tempDecodedChar, decodedChar,
-                        decodedPosition);
-                tempDecodedChar = decodedResult[0];
-                decodedChar = decodedResult[1];
-                decodedPosition = decodedResult[2];
+                decodedChar.readNextAllowedChar(decodedReader);
 
-                if (tempInputChar != tempDecodedChar) {
-                    System.out.printf("FAIL input %c @ %d output %c @ %d%n", (char) inputChar, inputPosition,
-                            (char) decodedChar, decodedPosition);
+                if (inputChar.temp != decodedChar.temp) {
+                    System.out.printf("FAIL input %c @ %d output %c @ %d%n", (char) inputChar.current,
+                            inputChar.position,
+                            (char) decodedChar.current, decodedChar.position);
                     return false;
                 }
             }
